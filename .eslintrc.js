b248eda63efb74bb2d826c7c97457b69c8eb192c
Artyom Danilin <artyom.danilin@orioninc.com>
2021-01-27 17:46:34 +0300

    add chakra ui for nuxt.js and new eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,37 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [],
-  // add your custom rules here
-  rules: {
-    'vue/html-indent': ['error', 4, {
-      attribute: 1,
-      baseIndent: 1,
-      closeBracket: 0,
-      alignAttributesVertically: true,
-      ignores: []
-    }],
-    'vue/html-closing-bracket-spacing': ['error', {
-      startTag: 'never',
-      endTag: 'never',
-      selfClosingTag: 'never'
-    }],
-    'vue/max-attributes-per-line': ['error', {
-      singleline: 1,
-      multiline: {
-        max: 2,
-        allowFirstLine: true
-      }
-    }]
-  }
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    extends: [
+        '@nuxtjs',
+        'plugin:nuxt/recommended'
+    ],
+    plugins: [],
+    rules: {
+        'vue/html-indent': ['error', 4, {
+            attribute: 1,
+            baseIndent: 1,
+            closeBracket: 0,
+            alignAttributesVertically: true,
+            ignores: []
+        }],
+        'vue/html-closing-bracket-spacing': ['error', {
+            startTag: 'never',
+            endTag: 'never',
+            selfClosingTag: 'never'
+        }],
+        'vue/max-attributes-per-line': ['error', {
+            singleline: 2,
+            multiline: {
+                max: 2,
+                allowFirstLine: true
+            }
+        }],
+        indent: ['error', 4]
+    }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,43 +1,44 @@
 import pkg from './package.json'
 
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: pkg.name,
-    htmlAttrs: { lang: 'en' },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: pkg.name,
+        htmlAttrs: { lang: 'en' },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: pkg.description }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+        'normalize.css/normalize.css'
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'normalize.css/normalize.css'
-  ],
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+    ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        '@nuxtjs/eslint-module'
+    ],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        '@chakra-ui/nuxt',
+        '@nuxtjs/emotion'
+    ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+    }
 }
