622edd2b2f1aa348f901e0e32979767f76ce2850
Artyom Danilin <artyom.danilin@orioninc.com>
2021-01-27 17:19:29 +0300

    clean up nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,14 @@
+import pkg from './package.json'
+
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'website',
-    htmlAttrs: {
-      lang: 'en'
-    },
+    title: pkg.name,
+    htmlAttrs: { lang: 'en' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
