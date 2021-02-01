490d2d0dce67d5b8f2402cc9f79774ab68b17a5a
Artyom Danilin <artyom.danilin@orioninc.com>
2021-02-01 12:50:40 +0300

    change main styles

--- config/custom-theme.js
+++ config/custom-theme.js
@@ -5,27 +5,26 @@ export default {
     ...defaultTheme,
     colors: {
         primary: '#fdf9f5',
-        secondary: '#e6a73c',
-        // generate palette: https://coolors.co/e6a73c-d4b57f-9c6e21-8c6c35-784f0a
-        marigold: '#e6a73c',
-        burlywood: '#d4b57f',
-        goldenBrown: '#9c6e21',
-        bistreBrown: '#8c6c35',
-        brown: '#784f0a',
+        secondary: '#593196',
+        // generate palette: https://coolors.co
+        purplePrimary: '#593196',
+        purpleSecondary: '#703EBC',
+        blackPrimary: '#0d1117',
+        blackSecondary: '#161b22',
 
         // generate brand colors: palx.jxnblk.com (base color: e6a73c)
         brand: {
             orange: {
-                50: '#fdf9f5',
-                100: '#f9ebe1',
-                200: '#f5dccb', // blck
-                300: '#f0ccb3',
-                400: '#eaba98',
-                500: '#e4a679', // li
-                600: '#dd8d54',
-                700: '#d36d24',
-                800: '#b54c00',
-                900: '#6b2d00'
+                50: '#faf9fc',
+                100: '#f0ecf5',
+                200: '#e4deee', // blck
+                300: '#d8cfe7',
+                400: '#cbbfde',
+                500: '#bcacd5', // li
+                600: '#ab97ca',
+                700: '#967ebd',
+                800: '#7b5dac',
+                900: '#4d2b84'
             }
         }
     }
