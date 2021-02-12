b574beee50ec7a1fc11f47f9c8c6a857572a8841
Artyom Danilin <kyza20106@yandex.ru>
2021-02-12 19:27:20 +0300

    post grabber add sort by date

--- plugins/blog_grabber.server.js
+++ plugins/blog_grabber.server.js
@@ -2,18 +2,37 @@ const fs = require('fs')
 const path = require('path')
 const parser = require('gray-matter')
 
-// start here
+function sortByDate (a, b) {
+    if (a.publishedAt < b.publishedAt) {
+        return 1
+    } else {
+        return -1
+    }
+}
+
 const blogPathSegment = 'pages/blog'
 const blogDir = path.join(process.cwd(), blogPathSegment)
 const fileNames = fs.readdirSync(blogDir)
 
 // collect metadata
-const postMetaData = fileNames.map((fileName) => {
-    const postFullPath = path.join(blogDir, fileName)
-    const content = fs.readFileSync(postFullPath, 'utf-8')
+const postMetadata = fileNames
+    .map((fileName) => {
+        const postFullPath = path.join(blogDir, fileName)
+        const content = fs.readFileSync(postFullPath, 'utf-8')
+        const postName = fileName.replace(/\.mdx$/, '')
 
-    const { data } = parser(content)
-    return data
-})
+        const { data } = parser(content)
+        return {
+            postName,
+            title: data.title,
+            publishedAt: data.publishedAt,
+            summary: data.summary,
+            image: data.image,
+            languageTags: data.languageTags,
+            isPopular: data.isPopular,
+            guides: data.guides
+        }
+    })
+    .sort(sortByDate)
 
-export default postMetaData
+export default postMetadata
