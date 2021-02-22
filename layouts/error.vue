691ffc9d3c9e8b2999b262ac8c2158d273cae576
Artyom Danilin <kyza20106@yandex.ru>
2021-02-22 15:01:49 +0300

    update error page to use mock component

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,47 +1,16 @@
 <template>
-    <c-flex align-items="center"
-            justify-content="center"
-            class="all-height"
-    >
-        <c-box mb="24">
-            <c-heading
-                as="h1" font-size="6xl"
-                mx="auto" mb="0"
-            >
-                <c-icon name="snowplow"/>
-                404
-            </c-heading>
-            <c-text
-                pt="0" mt="0"
-                font-size="md"
-                font-weight="semibold"
-            >
-                the page you are looking for is not found...
-            </c-text>
-            <nuxt-link to="/" mt="4">
-                <a-d-button size="sm">
-                    Go home
-                </a-d-button>
-            </nuxt-link>
-        </c-box>
-    </c-flex>
+    <a-d-mock icon="snowplow" status="4o4"/>
 </template>
 
 <script lang="js">
-import { CFlex, CBox, CIcon, CHeading, CText } from '@chakra-ui/vue'
-import ADButton from '~/components/common/buttons/ADButton'
+import ADMock from '~/components/ADMock'
 
 export default {
     name: 'Error',
     components: {
-        CFlex,
-        CHeading,
-        CIcon,
-        CText,
-        CBox,
-        ADButton
+        ADMock
     },
-    layout: 'error-layout'
+    layout: 'with-header'
 }
 </script>
 
