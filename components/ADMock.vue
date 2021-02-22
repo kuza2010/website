7551b29c0f3720515b9a6031f1b31f3b9a003e4c
Artyom Danilin <kyza20106@yandex.ru>
2021-02-22 15:01:18 +0300

    add mock component

---
+++ components/ADMock.vue
@@ -0,0 +1,59 @@
+<template>
+    <c-flex align-items="center"
+            justify-content="center"
+            class="all-height"
+    >
+        <c-box mb="24">
+            <c-heading
+                as="h1" font-size="6xl"
+                mx="auto" mb="0"
+            >
+                <c-icon :name="icon"/>
+                {{ status }}
+            </c-heading>
+            <c-text
+                pt="0"
+                font-size="md"
+                font-weight="semibold"
+            >
+                currently this page is in development... :(
+            </c-text>
+            <nuxt-link to="/" mt="4">
+                <a-d-button size="sm">
+                    Go home
+                </a-d-button>
+            </nuxt-link>
+        </c-box>
+    </c-flex>
+</template>
+
+<script>
+import { CFlex, CBox, CIcon, CHeading, CText } from '@chakra-ui/vue'
+import ADButton from '~/components/common/buttons/ADButton'
+
+export default {
+    name: 'ADMock',
+    components: {
+        CFlex,
+        CHeading,
+        CIcon,
+        CText,
+        CBox,
+        ADButton
+    },
+    layout: 'with-header',
+    // eslint-disable-next-line vue/require-prop-types
+    props: ['status', 'icon']
+}
+</script>
+
+<style scoped lang="scss">
+
+a {
+    text-decoration: none;
+}
+
+.all-height {
+    height: calc(100vh - 6rem)
+}
+</style>
